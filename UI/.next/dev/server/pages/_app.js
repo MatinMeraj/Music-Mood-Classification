ac52927837b79911f7d34f0e37191867fd21c217
Jaime Saraza <jms40@sfu.ca>
2025-11-29 23:16:34 -0800

    removed UI because i can't fix it

--- UI/.next/dev/server/pages/_app.js
+++
@@ -1,4 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/[root-of-the-server]__5a3af627._.js")
-R.m("[project]/OneDrive/Documents/School/fall'25/CMPT 310/Project/Spotify-Song-Classifier/UI/node_modules/next/app.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/OneDrive/Documents/School/fall'25/CMPT 310/Project/Spotify-Song-Classifier/UI/node_modules/next/app.js [ssr] (ecmascript)").exports
--- UI/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qBIans4QDlxejek82cpCFqLKeRfye3kFLWFUAS8OKoE=\"\n}"
